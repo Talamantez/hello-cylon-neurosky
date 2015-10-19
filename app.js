a121eb67344eab75f0a7ab8b2311673a9a5faaf5
Robert Talamantez <robert.m.talamantez@gmail.com>
2015-10-19 13:54:36 -0700

    hello world neurosky

---
+++ app.js
@@ -0,0 +1,24 @@
+var Cylon = require('cylon');
+
+Cylon.robot({
+ 
+  connections: {
+    neurosky: { 
+	adaptor: 'neurosky', 
+	port: '/dev/rfcomm0'
+    }
+  },
+
+  devices: {
+    headset: { 
+	driver: 'neurosky' 
+    }
+  },
+
+  work: function(my) {
+  	my.headset.on('packet', function(){
+  		console.log('packet recieved');
+  	});
+  }
+
+}).start();
